8f8312145ee8833e493446b21cd6c792978b335c
FYPYTHON <1823218990@qq.com>
2023-01-11 21:16:24 +0800

    postgres timeout

---
+++ component/openGauss/pyclient/test_timeout.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# 1823218990@qq.com
+
+import sys
+import psycopg2
+
+
+class Psycopg2Client(object):
+    def __init__(self, host="172.16.80.191", port=9242, password="Keda!Mysql_36", user="kedacom",
+                 database="postgres", timeout=10, logger=None):
+            self.sqlclient = psycopg2.connect(host=host, port=port, user=user, password=password,
+                                              database=database,
+                                              connect_timeout=timeout,
+                                              options='-c statement_timeout={}s'.format(timeout))
+            self.cursor = self.sqlclient.cursor()
+            self.sqlclient.autocommit = True
+            # self.cursor.execute("set statement_timeout='{}s'".format(timeout))
+
+    def run(self):
+        self.cursor.execute("select sleep 60;")
+
+
+if __name__ == '__main__':
+    client = ClientPsycopg2()
+    client.run()
