b7d0ca7ff2e469f978b20cee855e746dbb4dffe9
FYPYTHON <1823218990@qq.com>
2023-01-31 16:03:07 +0800

    python获取命令打印信息

--- python/笔记/python_get_return.py
+++ python/笔记/python_get_return.py
@@ -3,3 +3,9 @@
 import os
 res = os.system("percent_space=$(df /root | awk 'END{print $5}' | tr -d '%');exit $percent_space")
 code = res >> 8
+
+
+
+code, msg = subcommand("redis-benchmak 2>&1")
+# 命令带上 2>&1, 将命令打印信息输出到msg
+
