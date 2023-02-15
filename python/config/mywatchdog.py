ab16894ce5b76bb8ad48708179cf4b436fb24857
FYPYTHON <1823218990@qq.com>
2023-02-15 15:17:52 +0800

    snapshot方式监控文件变化

--- python/config/mywatchdog.py
+++ python/config/mywatchdog.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# 2023-02-14
 """
 https://pythonhosted.org/watchdog/quickstart.html
 """
@@ -10,6 +9,9 @@ import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+#
+import threading
+from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 
 
 def getLog(logfile="./mywatchdog.log", level=logging.DEBUG):
@@ -67,12 +69,73 @@ class MyWatchDog(FileSystemEventHandler):
                 self.fileNameList.remove(event.src_path)
 
     def on_modified(self, event):
+        """
+        windows 平台上每一次的文件修改引发两次 modified 事件 ?
+        :param event:
+        :return:
+        """
         print(event)
         self.logger.info(self.fileNameList)
         if event.is_directory:
             self.logger.info("directory modified:{0}".format(event.src_path))
         else:
             self.logger.info("file modified:{0}".format(event.src_path))
+
+
+class DiffWatchDog(FileSystemEventHandler):
+    """
+    snapshot 监控文件变化
+    """
+    def __init__(self, path):
+        print("init DiffWatchDog: {}".format(path))
+        FileSystemEventHandler.__init__(self)
+        self.snapshot = DirectorySnapshot(path)
+        self.timer = None
+        self.path = path
+
+    def heartbeat(self):
+        if self.timer:
+            # print("timer cancel")
+            self.timer.cancel()
+        self.check_snapshot()
+        self.timer = threading.Timer(0.2, self.heartbeat)
+        self.timer.start()
+        # print("on_any_event")
+
+    def check_snapshot(self):
+        snapshot = DirectorySnapshot(self.path)
+        diff = DirectorySnapshotDiff(self.snapshot, snapshot)
+        self.snapshot = snapshot
+        self.timer = None
+
+        if diff.files_modified:
+            print('files modified: {}'.format(diff.files_modified))
+        # print('files created: {}'.format(diff.files_created))
+        # print('files deleted: {}'.format(diff.files_deleted))
+        # print('files modified: {}'.format(diff.files_modified))
+        # print('files moved: {}'.format(diff.files_moved))
+
+        # print('dirs created: {}'.format(diff.dirs_created))
+        # print('dirs deleted: {}'.format(diff.dirs_deleted))
+        # print('dirs modified: {}'.format(diff.dirs_modified))
+        # print('dirs moved: {}'.format(diff.dirs_moved))
+
+
+class TimedDirWatchDog(object):
+    def __init__(self, path):
+        print("init TimedDirWatchDog")
+        self.path = path
+        self.observer = Observer()
+
+    def start(self):
+        diff_watchdog_handler = DiffWatchDog(self.path)
+        # self.observer.schedule(diff_watchdog_handler, self.path, recursive=True)
+        # self.observer.start()
+        # diff_watchdog_handler.check_snapshot()
+        diff_watchdog_handler.heartbeat()
+
+    def stop(self):
+        self.observer.stop()
 
 
 def run(path="."):
@@ -89,7 +152,13 @@ def run(path="."):
     observer.join()
 
 
+def monitor(path="."):
+    mt = DiffWatchDog(path)
+    mt.heartbeat()
+
+
 if __name__ == "__main__":
     # path = sys.argv[1] if len(sys.argv) > 1 else '.'
     path = "D:\project\lib\example\watch_file"
-    run(path)
+    # run(path)
+    monitor(path)
